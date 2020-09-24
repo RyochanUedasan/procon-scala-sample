9b1901ac0f3333acecfc1eb9e568bed52fb80dfa
RYOHEI UEDA <ueda_ryohei@cyberagent.co.jp>
2020-09-24 13:29:53 +0900

    begginerのやつにした

--- src/main/scala/A.scala
+++ src/main/scala/A.scala
@@ -1,9 +1,13 @@
 import scala.io.StdIn._
 
-// https://atcoder.jp/contests/abc178/tasks/abc178_a
+// https://atcoder.jp/contests/abs/tasks/abc086_a
 // 提出する時はobject名をMainにする
 object A extends App {
-  val x = readInt
+  val s: String = readLine
+  val seq: Seq[String] = s.split(" ")
+  val nums: Seq[Int] = seq.map(_.toInt)
+  val a = nums(0)
+  val b = nums(1)
   // ここに処理を書く
   println()
 }
--- src/main/scala/B.scala
+++ src/main/scala/B.scala
@@ -1,13 +1,8 @@
 import scala.io.StdIn._
 
-// https://atcoder.jp/contests/abc178/tasks/abc178_a
+// https://atcoder.jp/contests/abs/tasks/abc081_a
 // 提出する時はobject名をMainにする
 object B extends App {
-  val nums = readLine.split(" ").map(_.toInt)
-  val a = nums(0)
-  val b = nums(1)
-  val c = nums(2)
-  val d = nums(3)
   // ここに処理を書く
   println()
 }
